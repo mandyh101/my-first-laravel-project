7216bf4d9c38fb4ba6c4bb218d0c3e805b6073f9
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-08-23 12:58:49 +1200

    replace long path string with helper function resource_path

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ class Post
 {
   public static function find($slug)
   {
-    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
+    if(!file_exists($path = resource_path("posts/{$slug}.html"))){
       return redirect('/');
     }
 
