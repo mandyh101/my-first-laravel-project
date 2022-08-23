7dad776ef128819316766a1106333b73cf1712aa
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-08-23 13:01:03 +1200

    replace redirect with exception. It's not the job of Model classes to redirect

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ class Post
   public static function find($slug)
   {
     if(!file_exists($path = resource_path("posts/{$slug}.html"))){
-      return redirect('/');
+      throw new ModelNoteFoundException();
     }
 
     //cache page data in memory for 5 seconds
