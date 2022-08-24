b3b7e7586f99d20d4188b956ef6b97b7fd1be063
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-08-24 12:01:25 +1200

    replace vanilla php with blade template

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,15 +11,15 @@
   <?php foreach($posts as $post) : ?>
     <article>
       <h1>
-        <a href="/posts/<?= $post->slug; ?>">
-        <?= $post->title; ?>
+        <a href="/posts/{{$post->slug}}">
+        {{$post->title}}
         </a>
       </h1>
       <p>
         <em>Published on <?= date('m/d/Y', $post->date) ; ?></em>
       </p>
       <div>
-      <?= $post->excerpt; ?>
+      {{$post->excerpt}}
       </div>
     </article>
   <?php endforeach; ?>
