9a052dac63e3148470accb2998dad8a8f2691b1e
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-08-24 14:06:55 +1200

    create layout template and edit posts view as extension of layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Laracast blog</title>
-  <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
   @foreach($posts as $post)
     <article>
       <h1>
@@ -23,6 +16,4 @@
       </div>
     </article>
   @endforeach
-
-</body>
-</html>
+@endsection('content')
