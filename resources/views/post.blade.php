55ca4192fb315900c7fe15fd3a0cfbaa4e4bfed2
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-08-24 14:09:21 +1200

    -edit post view to extend layout template

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Laracast blog</title>
-  <link rel="stylesheet" href="/app.css">
-</head>
+@extends('layout')
+  @section('content')
+    <article>
+      <h1>{{$post->title}}</h1>
+      <div>
+        {!!$post->body!!}
+      </div>
+    </article>
 
-<body>
+    <a href="/">Go back</a>
+  
+  @endsection('content')
 
-  <article>
-    <h1>{{$post->title}}</h1>
-    <div>
-      {!!$post->body!!}
-    </div>
-  </article>
 
-  <a href="/">Go back</a>
-
-</body>
-
-</html>
