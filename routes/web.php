e9002bef0225f78db07acf712d9c827fb901c899
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-08-31 21:26:34 +1200

    clear comments

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,7 @@ Route::get('/', function () {
     ]);
 });
 
-//route model binding = if wildcard param matches the variable passed to the function, the program will find the default key that matches the wildcard variable and use it to find the content for the route.
-//give me the post where the value passed to the route matches the slug of a post
+
 Route::get("posts/{post}", function(Post $post) { 
-  //$post = Post::findOrFail($post);
-  return view('post', ['post' => $post]); //return view called post, pass the post to the view
+  return view('post', ['post' => $post]); 
 });
