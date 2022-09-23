d2ab6050bff152f424fb3251a781ad7324d4dc4b
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-23 16:09:25 +1200

    add link to authors page, remove duplicate category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -12,7 +12,9 @@
       </h2>
       <!-- code below used the eloquent relationship in the Post model to join the post and data table and show the posts matching category name -->
       <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a> 
-
+      <p>
+        By <a href="/authors/{{$post->author->id}}">{!!$post->author->name!!}</a> 
+      </p>
       <p class="{{$loop->even?'diff-bground':''}}">
         <em>Published on <?= date('m/d/Y', $post->date) ; ?></em>
       </p>
