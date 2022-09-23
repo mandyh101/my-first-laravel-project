9975f7cf034c8ef1c43bf8a77b3210b9f9669837
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-23 16:09:34 +1200

    create authors route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 // home page route
 Route::get('/', function () {
   return view('posts', [
-    'posts' => Post::latest()->with('category')->get()
+    'posts' => Post::latest()->with(['category', 'author'])->get()
     ]);
 });
 
@@ -30,5 +31,11 @@ Route::get("posts/{post}", function(Post $post) {
 Route::get("categories/{category:slug}", function(Category $category){
   return view('posts', [
     'posts'=> $category->posts 
+  ]);
+});
+
+Route::get("authors/{author}", function(User $author){
+  return view('posts', [
+    'posts'=> $author->posts 
   ]);
 });
