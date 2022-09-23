6fe6ec37669cb6e41e20785936c56f26d633482d
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-23 17:16:08 +1200

    update link to authors page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,11 +10,13 @@
         {{$post->title}}
         </a>
       </h2>
-      <!-- code below used the eloquent relationship in the Post model to join the post and data table and show the posts matching category name -->
-      <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a> 
       <p>
-        By <a href="/authors/{{$post->author->id}}">{!!$post->author->name!!}</a> 
+        By <a href="/authors/{{$post->author->username}}">{!!$post->author->username!!}</a> 
       </p>
+      <!-- code below used the eloquent relationship in the Post model to join the post and data table and show the posts matching category name -->
+      <p>
+        Category:<a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+      </p> 
       <p class="{{$loop->even?'diff-bground':''}}">
         <em>Published on <?= date('m/d/Y', $post->date) ; ?></em>
       </p>
