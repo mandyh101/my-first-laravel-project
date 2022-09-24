4720c7b6eaead3bf5dd0f31ed512405bddcf1409
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-25 10:43:29 +1300

    more style to dropdown div

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -18,12 +18,12 @@
       <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
         <div x-data="{show: false}">
 
-          <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold">Categories</button>
+          <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left">Categories</button>
 
-          <div x-show="show" class="py-2 absolute">
-            <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300">Two</a>
-            <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300">One</a>
-            <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300">Three</a>
+          <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-lg z-50" style="display:none">
+            <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white">Two</a>
+            <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white">One</a>
+            <a href="#" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white">Three</a>
           </div>
         </div>
 
