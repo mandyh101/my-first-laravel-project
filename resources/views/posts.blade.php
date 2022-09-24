f257b280eb512cc2b064d02b1a1c728768790bef
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-24 14:43:13 +1200

    extract the grid containing the posts into a reusable component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,17 +4,7 @@
   <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
     <!-- if posts have a count show the posts, else show a message -->
     @if ($posts->count())
-      <x-post-featured-card :post="$posts->first()"/>
-
-      @if ($posts->count() > 1)
-        <div class="lg:grid lg:grid-cols-6">
-          @foreach ($posts->skip(1) as $post)
-
-            <x-post-card :post="$post" class="{{$loop->iteration < 3? 'col-span-3' : 'col-span-2'}}"/>
-          @endforeach
-        </div>
-      @endif
-
+      <x-post-grid :posts="$posts"/>>
     @else
       <p class="text-center">Blog posts coming soon!</p>
     @endif
