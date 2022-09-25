b484002451b463b2480faa4059c5298356fd0863
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-26 11:17:10 +1300

    refactor to show current category name if selected

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -21,7 +21,7 @@
             @click="show = !show" 
             class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
           >
-            Categories
+            {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
             <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                 height="22" viewBox="0 0 22 22">
               <g fill="none" fill-rule="evenodd">
@@ -33,8 +33,17 @@
             </svg>
           </button>
             <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-lg z-50" style="display:none">
-            @foreach ($categories as $category)
-              <a href="/categories/{{$category->slug}}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white">{{ucwords($category->name)}}</a>
+              <a href="/" 
+                class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white"
+              >
+                All</a>
+              @foreach ($categories as $category)
+                <a href="/categories/{{$category->slug}}" 
+                  class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white
+                  {{isset($currentCategory) && $currentCategory->id === $category->id? 'bg-blue-500 text-white': ''}}
+                  "
+                >
+                  {{ucwords($category->name)}}</a>
               @endforeach
             </div>
         </div>
