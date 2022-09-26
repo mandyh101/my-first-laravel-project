62b1f90f99539b3a48bb039d0f5cc60129788cd5
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-26 13:24:28 +1300

    extract dropdown logic into a reusable component.

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,15 @@
+
+<div x-data="{show: false}" @click.away = "show = false">
+
+  <!-- trigger -->
+  <div @click="show = ! show">
+    {{$trigger}}
+  </div>
+
+
+  <!-- dropdown links -->
+
+  <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-lg z-50" style="">
+    {{$slot}}
+  </div>
+</div>
