e6bb880a0227881b2999fbc58a54f3dbe0e67a6c
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-09-26 13:43:19 +1300

    pass active state as props from _postsheader

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,5 +1,5 @@
 
-
+@props(['active' => false])
 <!-- refactor tailwind classes into a variable to tidy up HTML component -->
 @php
  $classes = 'block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-gray-300 hover:text-white focus:text-white';
