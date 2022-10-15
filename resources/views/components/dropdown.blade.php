a179cc11845d2227d771e077eb8ab1388c2caea3
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-10-16 11:36:47 +1300

    add overflow and max height to dropdown so the list doesn't get too long on the page

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -10,7 +10,7 @@
 
   <!-- dropdown links -->
 
-  <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-lg z-50" style="">
+  <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-lg z-50 overflow-auto max-h-52" style="">
     {{$slot}}
   </div>
 </div>
