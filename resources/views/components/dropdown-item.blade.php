da4355de8bd9592177f568cf3b233b120a7094c7
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-10-16 11:14:20 +1300

    comments

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,7 +1,7 @@
 
 @props(['active' => false])
 
-<!-- refactor tailwind classes into a variable to tidy up HTML component -->
+<!-- use php block refactor tailwind classes into a variable to tidy up HTML component -->
 
 @php
     $classes = 'block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white';
