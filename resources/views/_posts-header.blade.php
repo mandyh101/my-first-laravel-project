a991840218d490de650ea544634d2f56b36ec322
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-10-16 11:33:27 +1300

    export svg icon to seperate blade and make icon component able to conditionally display different icons

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -22,15 +22,7 @@
               class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
             >
               {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
-              <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                  height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-              </svg>
+            <x-svg-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
             </button>
           </x-slot>
 
@@ -41,8 +33,7 @@
           @foreach ($categories as $category)
           <x-dropdown-item 
             href="/categories/{{$category->slug}}" 
-            {{-- check url slug and make the category item that has a matching slug the active item --}}
-            :active="request()->is("categories/{$category->slug}")">
+            :active='request()->is("categories/{$category->slug}")'>
             {{ucwords($category->name)}}</x-dropdown-item>
           @endforeach
         </x-dropdown>
