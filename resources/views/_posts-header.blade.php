0080e9ac7c6bbbc250727dfe5428a7fe6db57582
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-10-16 11:14:04 +1300

    fix error and refactor code

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -36,11 +36,11 @@
 
           <!-- dropdown links -->
           <x-dropdown-item href="/">All</x-dropdown-item>
-          <!-- TODO debug required -->
+
           @foreach ($categories as $category)
           <x-dropdown-item 
             href="/categories/{{$category->slug}}" 
-            :active='request()->is("categories/{$category->slug}")'
+            :active="isset($currentCategory)&& $currentCategory->is($category)">
             {{ucwords($category->name)}}</x-dropdown-item>
           @endforeach
         </x-dropdown>
