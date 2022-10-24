d7db2ca289163a6dc3e5fcd5f3755c663c260ca4
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-10-24 18:26:25 +1300

    refactor category dropdown into class component

--- app/View/Components/CategoryDropdown.php
+++ app/View/Components/CategoryDropdown.php
@@ -16,7 +16,9 @@ class CategoryDropdown extends Component
     public function render()
     {
         return view('components.category-dropdown', [
-          'categories' => Category::all()
+          'categories' => Category::all(),
+          'currentCategory' => Category::firstWhere('slug', request('category'))
+
         ]);
     }
 }
--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+<x-dropdown>
+  <!-- dropdown trigger -->
+  <x-slot name="trigger">
+    <button 
+      class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
+    >
+      {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+    <x-svg-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
+    </button>
+  </x-slot>
+
+  <!-- dropdown links -->
+    {{-- edit component to access named route --}}
+  <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+
+  @foreach ($categories as $category)
+  <x-dropdown-item 
+    href="/?category={{$category->slug}}" 
+    :active='request()->is("categories/{$category->slug}")'>
+    {{ucwords($category->name)}}</x-dropdown-item>
+  @endforeach
+</x-dropdown>
