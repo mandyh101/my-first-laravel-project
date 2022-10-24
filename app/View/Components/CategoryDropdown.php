e174eabf3f956c594260613077886c741ffe5aba
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-10-24 18:10:53 +1300

    pass categories to component and remove constructor

--- app/View/Components/CategoryDropdown.php
+++ app/View/Components/CategoryDropdown.php
@@ -2,19 +2,11 @@
 
 namespace App\View\Components;
 
+use App\Models\Category;
 use Illuminate\View\Component;
 
 class CategoryDropdown extends Component
 {
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
 
     /**
      * Get the view / contents that represent the component.
@@ -23,6 +15,8 @@ class CategoryDropdown extends Component
      */
     public function render()
     {
-        return view('components.category-dropdown');
+        return view('components.category-dropdown', [
+          'categories' => Category::all()
+        ]);
     }
 }
