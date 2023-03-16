73af26b8954d0c12f4dcc619b528a42095e53691
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-17 08:16:25 +1300

    add pagination bar and links for posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,9 @@
   <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
     <!-- if posts have a count show the posts, else show a message -->
     @if ($posts->count())
-      <x-post-grid :posts="$posts"/>>
+    <x-post-grid :posts="$posts"/>>
+    {{-- $posts are paginated so we can use the links method to show nd link to the paginated pages - NOTE this is auto styled with tailwind --}}
+    {{$posts->links()}}
     @else
       <p class="text-center">Blog posts coming soon!</p>
     @endif
