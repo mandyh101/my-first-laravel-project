d8c3deaee9ecc191bf2c1b4624f334c1b6477d0c
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 15:17:34 +1300

    start building register form

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,5 +1,71 @@
 <x-layout>
   <section class="px-6 py-8">
-    <h1>Hello world</h1>
+    <main class="max-w-lg mx-auto mt-10 bg-blue-50 rounded-xl border border-blue-100 p-6">
+      <h1 class="text-center font-bold text-xl">Register</h1>
+      <form method="POST" action="/register">
+        <div class="mb-6">
+          <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                  for="name"
+          >
+          Name
+          </label>
+          <input 
+            class="border border-gray-400 p-2 w-full"
+            type="text"
+            name="name"
+            id="name"
+            required
+          >
+        </div>
+        <div class="mb-6">
+          <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                  for="username"
+          >
+          Username
+          </label>
+          <input 
+            class="border border-gray-400 p-2 w-full"
+            type="text"
+            name="username"
+            id="username"
+            required
+          >
+        </div>
+        {{-- <div class="mb-6">
+          <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                  for="email"
+          >
+          Email
+          </label>
+          <input 
+            class="border border-gray-400 p-2 w-full"
+            type="text"
+            name="email"
+            id="email"
+            required
+          >
+        </div> --}}
+        <div class="mb-6">
+          <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                  for="password"
+          >
+          Password
+          </label>
+          <input 
+            class="border border-gray-400 p-2 w-full"
+            type="password"
+            name="password"
+            id="password"
+            required
+          >
+        </div>
+        <div class="mb-6">
+          <button type="submit" class="bg-blue-600 text-white rounded py-2 px-4 hover:bg-blue-700">
+            Submit
+          </button>
+
+        </div>
+      </form>
+    </main>
   </section>
 </x-layout>
