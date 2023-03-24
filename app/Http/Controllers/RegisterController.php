e8b9902e15fde10822cb21f0f20c14a11de9e8bc
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 17:16:44 +1300

    pass flash message with redirect

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -24,9 +24,7 @@ class RegisterController extends Controller
       //create the user with validated attributes if validation passes
       User::create($attributes);
 
-      session()->flash('success', 'Your account has been created!');
-
-      return redirect('/');
+      return redirect('/')->with('success', 'Your account has been created!');
     }
 
 }
