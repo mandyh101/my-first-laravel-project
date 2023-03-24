df5a698eb7a074f8777486b2ae5fc7bc96047b87
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 18:13:26 +1300

    pseudocode login store method

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,22 +7,34 @@ use Illuminate\Http\Request;
 class SessionController extends Controller
 {
 
-      /**
-       * logs in a user
-       */
-      public function create()
-      {
-        return view('sessions.create') ;
-      }
+  /**
+   * takes a user to the login from 
+   */
+  public function create()
+  {
+    return view('sessions.create') ;
+  }
 
-    /**
-     * logs out a user
-     */
-    public function destroy()
-    {
-      //logout the logged in user
-      auth()->logout();
+  /**
+   * logs in a user based on provided credentials
+   */
+  public function store(Request $request)
+  {
+    //validate the request
 
-      return redirect('/')->with('success', 'Goodbye!');
-    }
+    //login based on credentials
+
+    //redirect with a session message
+  }
+
+  /**
+   * logs out a user
+   */
+  public function destroy()
+  {
+    //logout the logged in user
+    auth()->logout();
+
+    return redirect('/')->with('success', 'Goodbye!');
+  }
 }
