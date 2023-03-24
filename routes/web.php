d44b9dc4b1ccd0b1c554bc2f824e53ba11d012c6
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 18:13:00 +1300

    add second login endpoint to create the session

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,8 @@ Route::get("posts/{post}", [PostController::class, 'show' ]);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('login', [SessionController::class, 'create'])->middleware('guest');;
+//login endpoints
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionController::class, 'store'])->middleware('guest');;
+
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');;
