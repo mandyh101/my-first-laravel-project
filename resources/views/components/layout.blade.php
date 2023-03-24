6a6cfe4bc595762b7bbfb997a89a5af006ddffe5
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 17:15:42 +1300

    create component for session flash messages

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,14 +56,6 @@
     </section>
 
     @if(session()->has('success'))
-      <div 
-        x-data="{show:true}"
-        x-init="setTimeout(() => show = false, 4000)"
-        x-show="show"
-        class="fixed bg-green-300 text-white py-2 px-4 rounded-xl bottom-3 right-3 text-sm">
-        <p>
-          {{session('success')}}
-        </p>
-      </div>
+    <x-flash />
     @endif
 </body>
