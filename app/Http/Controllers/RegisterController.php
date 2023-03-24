667acf557364412cc022e346243420b6469c2d5b
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 16:59:33 +1300

    emails unique too

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -17,7 +17,7 @@ class RegisterController extends Controller
       $attributes = $request->validate([
         'name' => ['required', 'string', 'max:255'],
         'username' => ['required', 'string', 'min:5' , 'max:25', 'unique:users,username'], //you can pass a third param to the unique which is a variable to ignore e.g the user id
-        'email' => ['required', 'email', 'max:255'],
+        'email' => ['required', 'email', 'max:255', 'unique:users,email'],
         'password' => ['required', 'min:8','max:255'],
       ]);
 
