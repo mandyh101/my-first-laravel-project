d62504a720577d91523aeebd885e534ef54a15e7
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-24 15:28:22 +1300

    add route and start building controller action for POST

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,4 +10,16 @@ class RegisterController extends Controller
     {
       return view('register.create');
     }
+
+    public function store(Request $request)
+    {
+      $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'username' => ['required', 'string', 'max:25', 'min:8'],
+        'email' => ['required', 'email', 'max:255'],
+        'password' => ['required', 'min:8', 'max:255'],
+      ]);
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/', [PostController::class, 'index' ])->name('home');
 Route::get("posts/{post}", [PostController::class, 'show' ]);
 
 Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
