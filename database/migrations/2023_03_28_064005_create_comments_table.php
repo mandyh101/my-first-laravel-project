97404d23d4fdcf2a146bae9b6b64eeed2969a098
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-28 19:51:20 +1300

    short way to crete a foreign key

--- database/migrations/2023_03_28_064005_create_comments_table.php
+++ database/migrations/2023_03_28_064005_create_comments_table.php
@@ -16,13 +16,12 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('post_id');
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
 
             $table->text('body');
             $table->timestamps();
 
             $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
