fd164a016c37b0796cde010e7f87582c670c059f
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-29 06:46:30 +1300

    build factory tp populate dummy comments

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -11,13 +13,16 @@ class CommentFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
+     * this factory will create a new post and user and put their ids into the comment instance
+     * this factory also creates a paragraph for the body attribute of a comment
      * @return array<string, mixed>
      */
     public function definition()
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
