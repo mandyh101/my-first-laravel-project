54781a131f14762ed3d82a5c8714f58207bddb18
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-29 06:57:50 +1300

    update styling and make avatar unique

--- resources/views/components/post-comment-card.blade.php
+++ resources/views/components/post-comment-card.blade.php
@@ -2,7 +2,7 @@
 
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
   <div>
-    <img class="rounded-xl" src="https://i.pravatar.cc/50" alt="avatar">
+    <img src="https://i.pravatar.cc/60?u={{$comment->id}}" width="60" height="60" class="rounded-xl" alt="avatar">
   </div>
   <div>
     <header class="mb-4">
