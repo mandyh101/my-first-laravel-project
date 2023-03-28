0b2590e1845766f94127e38f176618912fac0826
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2023-03-29 06:47:38 +1300

    accept comments as props

--- resources/views/components/post-comment-card.blade.php
+++ resources/views/components/post-comment-card.blade.php
@@ -1,3 +1,5 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
   <div>
     <img class="rounded-xl" src="https://i.pravatar.cc/50" alt="avatar">
